e5daf5e26bfab93d8d2cdc6bc334c080cfd8ccdf
Joey deVilla <joey@joeydevilla.com>
2021-11-21 15:29:18 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Jose Martin de Villa on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
