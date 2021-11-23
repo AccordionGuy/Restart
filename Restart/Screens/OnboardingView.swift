7df212d3455ba29b9059afb0a26a8da3581ee50e
Joey deVilla <joey@joeydevilla.com>
2021-11-23 10:12:27 -0500

    Enabled slide-to-activate control

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -18,6 +18,10 @@ struct OnboardingView: View {
   // this assignment is ignored.
   @AppStorage("onboarding") var isOnboardingViewActive = true
   
+  @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+  @State private var buttonOffset: CGFloat = 0
+  
+  
   var body: some View {
     ZStack {
       Color("ColorBlue")
@@ -60,7 +64,7 @@ struct OnboardingView: View {
         
         Spacer()
         
-        // Mark: Footer
+        // MARK: Footer
         
         ZStack {
           
@@ -87,7 +91,7 @@ struct OnboardingView: View {
           HStack {
             Capsule()
               .fill(Color("ColorRed"))
-              .frame(width: 80)
+              .frame(width: buttonOffset + 80)
             
             Spacer()
           }
@@ -106,15 +110,29 @@ struct OnboardingView: View {
             }
             .foregroundColor(.white)
             .frame(width: 80, height: 80, alignment: .center)
-            .onTapGesture {
-              isOnboardingViewActive = false
-            }
+            .offset(x: buttonOffset)
+            .gesture(
+              DragGesture()
+                .onChanged { gesture in
+                  if gesture.translation.width > 0  && buttonOffset <= buttonWidth - 80 {
+                    buttonOffset = gesture.translation.width
+                  }
+                } // onChanged()
+                .onEnded { _ in
+                  if buttonOffset > buttonWidth / 2 {
+                    buttonOffset = buttonWidth - 80
+                    isOnboardingViewActive = false
+                  } else {
+                    buttonOffset = 0
+                  }
+                }
+            )
             
             Spacer()
           }
           
         } // ZStack (Footer)
-        .frame(height:80, alignment: .center)
+        .frame(width: buttonWidth, height:80, alignment: .center)
         .padding()
         
       } // VStack
